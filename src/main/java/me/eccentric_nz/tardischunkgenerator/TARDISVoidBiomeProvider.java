301669ddc978b6d005577263fd56314fe79fe348
DalekCraft2 <ericdalekcraft@gmail.com>
2021-08-27 13:15:48 -0400

    Use overridden parameters

--- src/main/java/me/eccentric_nz/tardischunkgenerator/TARDISVoidBiomeProvider.java
+++ src/main/java/me/eccentric_nz/tardischunkgenerator/TARDISVoidBiomeProvider.java
@@ -4,18 +4,19 @@ import com.google.common.collect.Lists;
 import org.bukkit.block.Biome;
 import org.bukkit.generator.BiomeProvider;
 import org.bukkit.generator.WorldInfo;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class TARDISVoidBiomeProvider extends BiomeProvider {
 
     @Override
-    public Biome getBiome(WorldInfo worldInfo, int i, int i1, int i2) {
+    public Biome getBiome(@NotNull WorldInfo worldInfo, int x, int y, int z) {
         return Biome.THE_VOID;
     }
 
     @Override
-    public List<Biome> getBiomes(WorldInfo worldInfo) {
+    public List<Biome> getBiomes(@NotNull WorldInfo worldInfo) {
         return Lists.newArrayList(Biome.THE_VOID);
     }
 }
